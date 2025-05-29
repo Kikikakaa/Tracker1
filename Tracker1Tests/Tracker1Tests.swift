10c66885507c9d728ad02c0f8bf4765debc637e1
Никита <uzer591@gmail.com>
2025-05-29 13:39:20 +0500

    Initial Commit

---
+++ Tracker1Tests/Tracker1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker1Tests.swift
+//  Tracker1Tests
+//
+//  Created by user on 29.05.2025.
+//
+
+import Testing
+@testable import Tracker1
+
+struct Tracker1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
